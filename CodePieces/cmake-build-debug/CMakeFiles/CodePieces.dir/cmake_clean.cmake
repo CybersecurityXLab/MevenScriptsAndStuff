473d89f7452abfca108d1c6bb24a8a21106c5c6e
root <root@localhost.localdomain>
2018-05-18 12:46:42 -0400

    Add Pieces of the code from Mirai for futher analysis

---
+++ CodePieces/cmake-build-debug/CMakeFiles/CodePieces.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodePieces.dir/main.c.o"
+  "CodePieces.pdb"
+  "CodePieces"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CodePieces.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
