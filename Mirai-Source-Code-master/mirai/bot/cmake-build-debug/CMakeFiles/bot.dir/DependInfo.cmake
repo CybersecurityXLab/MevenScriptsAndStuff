c26c55692d6c1ad8594ac16f6668ad61dd663529
root <root@localhost.localdomain>
2018-05-18 12:48:31 -0400

    Add Mirai code with comments on scanner.c

---
+++ Mirai-Source-Code-master/mirai/bot/cmake-build-debug/CMakeFiles/bot.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/root/Documents/Mirai-Source-Code-master/mirai/bot/attack.c" "/root/Documents/Mirai-Source-Code-master/mirai/bot/cmake-build-debug/CMakeFiles/bot.dir/attack.c.o"
+  "/root/Documents/Mirai-Source-Code-master/mirai/bot/attack_app.c" "/root/Documents/Mirai-Source-Code-master/mirai/bot/cmake-build-debug/CMakeFiles/bot.dir/attack_app.c.o"
+  "/root/Documents/Mirai-Source-Code-master/mirai/bot/attack_gre.c" "/root/Documents/Mirai-Source-Code-master/mirai/bot/cmake-build-debug/CMakeFiles/bot.dir/attack_gre.c.o"
+  "/root/Documents/Mirai-Source-Code-master/mirai/bot/attack_tcp.c" "/root/Documents/Mirai-Source-Code-master/mirai/bot/cmake-build-debug/CMakeFiles/bot.dir/attack_tcp.c.o"
+  "/root/Documents/Mirai-Source-Code-master/mirai/bot/attack_udp.c" "/root/Documents/Mirai-Source-Code-master/mirai/bot/cmake-build-debug/CMakeFiles/bot.dir/attack_udp.c.o"
+  "/root/Documents/Mirai-Source-Code-master/mirai/bot/checksum.c" "/root/Documents/Mirai-Source-Code-master/mirai/bot/cmake-build-debug/CMakeFiles/bot.dir/checksum.c.o"
+  "/root/Documents/Mirai-Source-Code-master/mirai/bot/killer.c" "/root/Documents/Mirai-Source-Code-master/mirai/bot/cmake-build-debug/CMakeFiles/bot.dir/killer.c.o"
+  "/root/Documents/Mirai-Source-Code-master/mirai/bot/main.c" "/root/Documents/Mirai-Source-Code-master/mirai/bot/cmake-build-debug/CMakeFiles/bot.dir/main.c.o"
+  "/root/Documents/Mirai-Source-Code-master/mirai/bot/rand.c" "/root/Documents/Mirai-Source-Code-master/mirai/bot/cmake-build-debug/CMakeFiles/bot.dir/rand.c.o"
+  "/root/Documents/Mirai-Source-Code-master/mirai/bot/resolv.c" "/root/Documents/Mirai-Source-Code-master/mirai/bot/cmake-build-debug/CMakeFiles/bot.dir/resolv.c.o"
+  "/root/Documents/Mirai-Source-Code-master/mirai/bot/scanner.c" "/root/Documents/Mirai-Source-Code-master/mirai/bot/cmake-build-debug/CMakeFiles/bot.dir/scanner.c.o"
+  "/root/Documents/Mirai-Source-Code-master/mirai/bot/table.c" "/root/Documents/Mirai-Source-Code-master/mirai/bot/cmake-build-debug/CMakeFiles/bot.dir/table.c.o"
+  "/root/Documents/Mirai-Source-Code-master/mirai/bot/util.c" "/root/Documents/Mirai-Source-Code-master/mirai/bot/cmake-build-debug/CMakeFiles/bot.dir/util.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
