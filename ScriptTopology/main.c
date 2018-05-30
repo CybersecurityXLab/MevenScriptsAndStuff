b7bd08e8d571a160538891297df8dddb7ba75984
root <root@localhost.localdomain>
2018-05-30 12:23:31 -0400

    Add more explicit conditions to run the script

--- ScriptTopology/main.c
+++ ScriptTopology/main.c
@@ -9,9 +9,17 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "usage : %s subnetNumber numberOfDeviceOnSubnet\n", argv[0]);
         exit(1);
     }
+    
+    int arg1 = atoi(argv[1]);
+    int arg2 = atoi(argv[2]);
 
-    if(atoi(argv[1]) < 2 ){ // avoid negative network
-        fprintf(stderr, "please select a positive integer - subnet 1 is reserved for the monitor\n");
+    if(arg1 < 1 || arg2 < 1){ // avoid negative network
+        fprintf(stderr, "please select a number > 1 for the subnet number and at least 1 device\n");
+        exit(1);
+    }
+
+    if(arg1 == 1){
+        printf("This network is reserved for the monitor\n");
         exit(1);
     }
 
@@ -24,21 +32,16 @@ int main(int argc, char* argv[]) {
     strcat(filename, ".sh");
     file = fopen(filename, "w");
 
-
-
     if(file == NULL){ //check if the file is opened
         fprintf(stderr, "Error opening file");
         exit(1);
     }
 
-    int arg1 = atoi(argv[1]);
-    int arg2 = atoi(argv[2]);
-
     fprintf(file, "printf \""); //start writing the commands needed for the file executed on the ovs
-
     //ifconfig network on subnetX.sh + ifconfig devices on output lines
     fprintf(file, "sudo ifconfig eth1 10.10.%d.1 netmask 255.255.0.0\\n", arg1);
     printf("please enter the followings commands on the corresponding device of the subnet : \n");
+
     for(int i = 2; i <= arg2+1; i++){
         fprintf(file, "sudo ifconfig eth%d 10.10.%d.%d netmask 255.255.0.0\\n", i, arg1, i*2-1); //config network
         printf("device #%d : sudo ifconfig eth1 10.10.%d.%d netmask 255.255.0.0\n", i-1, arg1, i*2-2); // config device
@@ -48,12 +51,11 @@ int main(int argc, char* argv[]) {
     //setup for the bridge
     fprintf(file, "sudo ovs-vsctl add-br br0\\n"
                   "sudo ovs-vsctl set-fail-mode br0 standalone\\n");
-
     fprintf(file,"sudo ovs-vsctl add-port br0 eth1\\n");
+
     for(int i = 1; i <= arg2; i++){
         fprintf(file, "sudo ovs-vsctl add-port br0 eth%d\\n", i);
     }
-
 
     //create the bridge towards monitor's network
     fprintf(file, "sudo ovs-vsctl -- set Bridge br0 mirrors=@m -- ");
@@ -63,21 +65,21 @@ int main(int argc, char* argv[]) {
 
     fprintf(file, "-- --id=@m create Mirror name=mymirrorsub%d select-dst-port=", arg1);
 
-    for(int i = 2; i <= arg2+1; i++) {
-        if(i-1 == arg2){
-            fprintf(file, "@eth%d", i);
+    for(int i = 1; i <= arg2; i++) {
+        if(i == arg2){
+            fprintf(file, "@eth%d", i+1);
         }else{
-            fprintf(file, "@eth%d,", i);
+            fprintf(file, "@eth%d,", i+1);
         }
     }
 
     fprintf(file, " select-src-port=");
 
-    for(int i = 2; i-1 <= arg2; i++) {
-        if(i-1 == arg2){
-            fprintf(file, "@eth%d", i);
+    for(int i = 1; i <= arg2; i++) {
+        if(i == arg2){
+            fprintf(file, "@eth%d", i+1);
         }else{
-            fprintf(file, "@eth%d,", i);
+            fprintf(file, "@eth%d,", i+1);
         }
     }
 
